98dbd3e3fcfc5ec174975d8d3a8d407030dc1a89
BastianGH <bastianpussey16@gmail.com>
2023-02-26 04:50:51 +0100

    commencement cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,2 +1,14 @@
 console.clear();
 console.log("ici le cart.js")
+
+let cart = JSON.parse(window.localStorage.getItem('panier'))
+let cartSection = document.getElementById('cart__items')
+
+
+for( let i=0; i<cart.length; i++) {
+    console.log(cart[i])
+
+    let article = document.createElement('article');
+
+    cartSection.appendChild(article)
+}
