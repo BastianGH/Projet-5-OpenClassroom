41a95da24c5470d2ae864312c0549a42b603015f
BastianGH <bastianpussey16@gmail.com>
2023-02-26 04:31:27 +0100

    Page produit terminé

---
+++ front/js/cart.js
@@ -0,0 +1,2 @@
+console.clear();
+console.log("ici le cart.js")
--- front/js/product.js
+++ front/js/product.js
@@ -7,7 +7,9 @@ let title = document.getElementById('title');
 let price = document.getElementById('price'); 
 let description = document.getElementById('description'); 
 let options = document.getElementById('colors'); 
+let quantityInput = document.getElementById('quantity')
 let button = document.getElementById('addToCart');
+let chosenColor
 
 /*  On sélectionne l'ID se trouvant dans l'URL, que nous avons passé précédemment */
 let params = new URLSearchParams(document.location.search);
@@ -18,7 +20,7 @@ let id = params.get('id');
 */
 async function getArticle() {
     await  fetch("http://localhost:3000/api/products/"+id)
-    .then( function(response) { return response.json(); })
+    .then( function(response) { return response.json() })
     .then( function(data) { return product = data } )    
 };
 
@@ -53,44 +55,72 @@ async function createArticle() {
     }
 }
 
-
 /*  On appelle la fonction pour créer l'article */
 createArticle();
 
+/*  On écoute le changement d'option sur le "bouton select" pour connaitre la couleur choisie par le client */
+colors.addEventListener('change', function() {
+    chosenColor = colors.value
+});
+
 /*  On met une écoute d'évènement au click sur le bouton "Ajouter au panier" 
-    On initialise une nouvelle variable, qui contiendra toutes les infos
+    On initialise une nouvelle variable, qui contiendra toutes les infos du produit
     On sélectionne l'Id du produit actuelle, la couleur et la quantité choisie
+    On oublie pas de convertir la quantité en Integer pour la suite
     On met toutes ces valeur dans notre nouvel objet
 
-    Ensuite on veut mettre notre objet dans le localStorage pour représenter le panier :
-        Si le produit concerné y est déja :
-            On vérifie si la couleur de celui-ci est la même que celle sélectionné :
-                Si oui, on incrémente seulement la quantité de notre produit;
+    Ensuite on veut représenter notre panier dans le localStorage,
+    On va dans un premier temps vérifier si le panier est déja crée,
+    On vérifie également que l'utilisateur a bien choisi une couleur :
+        Si le panier existe déja : 
+            On vérifie si le produit concerné y est déja,
+            Pour cela on boucle sur notre Array 'panier'
+            Si le produit et sa couleur sont la même que celle sélectionné :
+                On incrémente seulement la quantité de notre produit;
                 Si non, on crée un nouveau produit;
-        Sinon, si le produit concerné n'y est pas encore, on l'ajoute simplement dans le panier
+            Sinon, si le produit concerné n'y est pas encore,
+            On l'ajoute simplement dans le panier
+        Si le panier n'existe pas encore, on initialise notre objet,
+        On lui push le produit voulu et on initialise notre panier dans le localStorage        
 */
 button.addEventListener('click',function () {
     let newProduct = {};
-    let quantityInput = document.getElementById('quantity')
     let newQuantity = parseInt(quantityInput.value);
-    // let color = option.value;
+    newProduct.id = product._id
+    newProduct.quantity = newQuantity
+    newProduct.color = chosenColor  
+        
+    let existingCart = JSON.parse(window.localStorage.getItem('panier'))
 
-    // newProduct.push(color)
-   
-    let addedProduct = JSON.parse(window.localStorage.getItem(product._id))
-    console.log(addedProduct)
-
-    if(addedProduct) {
-        let oldQuantity = parseInt(addedProduct.quantity)
-        newProduct.quantity = oldQuantity+newQuantity
-        newProduct = JSON.stringify(newProduct);
-        window.localStorage.setItem(product._id, newProduct)
-        console.log("La quantité a été mise à jour")
-    }else { 
-        newProduct.quantity = newQuantity
-        newProduct = JSON.stringify(newProduct);
-        window.localStorage.setItem(product._id, newProduct)
-        console.log("Le produit a été ajouté au panier")
+    if(chosenColor) {
+        if(existingCart) {
+            let addedProduct = false;
+            let oldId
+    
+            for( let i=0; i<existingCart.length; i++ ){    
+                if(existingCart[i].id===newProduct.id && existingCart[i].color===newProduct.color){
+                    addedProduct = true;
+                    oldId = i;
+                }
+            }
+            
+            if(addedProduct){
+                existingCart[oldId].quantity += newProduct.quantity;
+                console.log(existingCart)
+                existingCart = JSON.stringify(existingCart);
+                window.localStorage.setItem('panier', existingCart)
+            }else {
+                existingCart.push(newProduct)
+                existingCart = JSON.stringify(existingCart);
+                window.localStorage.setItem('panier', existingCart)
+            }
+        }else { 
+            let panier = [];
+            panier.push(newProduct)
+            panier = JSON.stringify(panier);
+            window.localStorage.setItem('panier', panier)
+        }
     }
+    //window.localStorage.removeItem('panier')
 });
 
